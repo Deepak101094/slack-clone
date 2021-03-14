ddfaa0bb3efd39e29adf909a2553ec18e1800784
Deepak101094 <deepakpandey101094@gmail.com>
2021-03-15 01:24:13 +0530

    styling in aapbar using styled component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import Header from  "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-     <h3>slack-clone</h3>
+    <div>
+      <Router>
+        <>
+        <Switch>
+          <Route exact={true} path="/">
+            <Header />
+          </Route>
+        </Switch>
+        </>
+      </Router>
     </div>
   );
 }
