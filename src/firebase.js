05c333a112f44fbd87aac154461e29ae24802710
Deepak101094 <deepakpandey101094@gmail.com>
2021-03-14 23:56:54 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA-h6TBdSFMbLGbeEVjnKZq7TzdgSsgu-I",
+    authDomain: "slack-clone-1bf09.firebaseapp.com",
+    projectId: "slack-clone-1bf09",
+    storageBucket: "slack-clone-1bf09.appspot.com",
+    messagingSenderId: "387673965636",
+    appId: "1:387673965636:web:5d203dad4e1e61da2be811"
+  };
